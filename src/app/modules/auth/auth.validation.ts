477f42caf751b991df13f1c594d0413f8d533cc4
Shahariar Sohan <Sohanshahariar4@gmail.com>
2025-09-04 10:17:30 +0600

    zod schema added for forget and reset password route

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -8,8 +8,14 @@ export const forgetPasswordZodSchema = z.object({
 });
 export const resetPasswordZodSchema = z.object({
   id: z.string(),
-  email: z
-    .email({ message: "Email is invalid" })
-    .min(5, { message: "Email must be at least 5 characters" })
-    .max(100, { message: "Email must be maximum of 100 characters" }),
+  newPassword: z
+    .string({ error: " Password must be string" })
+    .min(8, { message: "Password must be at least 8 characters" })
+    .regex(/.*[A-Z].*/, {
+      message: `Password must be at least 1 uppercase letter`,
+    })
+    .regex(/.*\d.*/, { message: `Password must be at least 1 number` })
+    .regex(/[!@#$%^&*?]/, {
+      message: `Password must be at least 1 special character`,
+    }),
 });
